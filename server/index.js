7d7a642d2d4e5f63376d5f3d2a3cec9bc344d415
GiFree <fra.matusiak@gmail.com>
2018-08-09 03:01:36 +0200

    prepared express static file serving

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const logger = require('morgan');
+const path = require('path');
 const socketConfig = require('./socket.js');
 
 const app = express();
@@ -17,8 +18,14 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 app.use(logger('dev'));
 
+app.use(express.static(path.join(__dirname, '../dist')));
+
 const routes = require('./routes');
 app.use('/', routes);
+
+app.get('*', (req, res, next) => {
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
+})
 
 app.use((err, req, res, next) => {
   res.status(err.status || 500);
